ca05233887092657a1ad2043006e08d7c672d839
Feng <feng.zhu1980@gmail.com>
2020-05-13 11:16:57 +1200

    Add checkout function...

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -1,9 +1,11 @@
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CheckoutComponent } from './checkout.component';
 
 const routes: Routes = [
-  {path: '', component: CheckoutComponent}
+  {path: '', component: CheckoutComponent},
+  {path: 'success', component: CheckoutSuccessComponent}
 ];
 
 @NgModule({
